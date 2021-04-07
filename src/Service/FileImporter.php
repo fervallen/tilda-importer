e706c8060eb1ad2b1e7121fab2017fd7d9ff5798
Pavel Prykhodko <mtk@fervallen.com>
2021-04-07 22:50:29 +0300

    Fix new bug with Tilda incorrect page

--- src/Service/FileImporter.php
+++ src/Service/FileImporter.php
@@ -8,6 +8,7 @@ use TildaTools\Tilda\Objects\Asset;
 
 class FileImporter
 {
+    private const TILDA_INCORRECT_PATH_MASK = '/https:\/\/tilda\.ws\/project(\d+)\/tilda-blocks-([0-9.]+)(js|css)/';
     private string $publicDirectory;
 
     public function __construct(KernelInterface $kernel)
@@ -17,15 +18,20 @@ class FileImporter
 
     public function import(Asset $asset, string $directory): void
     {
-        $directory = $this->publicDirectory . $directory;
-        if (!file_exists($directory) && !mkdir($directory) && !is_dir($directory)) {
-            throw new RuntimeException(sprintf('Directory "%s" was not created', $directory));
+        $directoryPath = $this->publicDirectory;
+        if (!preg_match(self::TILDA_INCORRECT_PATH_MASK, $asset->from) &&
+            (strpos($asset->to, $directory) !== 0)
+        ) {
+            $directoryPath .= $directory;
+        }
+        if (!file_exists($directoryPath) && !mkdir($directoryPath) && !is_dir($directoryPath)) {
+            throw new RuntimeException(sprintf('Directory "%s" was not created', $directoryPath));
         }
 
         $content = file_get_contents($asset->from);
         if (!$content) {
             throw new RuntimeException(sprintf('Asset "%s" is empty', $asset->from));
         }
-        file_put_contents($directory . $asset->to, $content);
+        file_put_contents($directoryPath . $asset->to, $content);
     }
 }
