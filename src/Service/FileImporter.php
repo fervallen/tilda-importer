086e671283c7424da1fb979e3871319bd3e1b6e5
Pavel Prykhodko <mtk@fervallen.com>
2023-03-06 13:57:32 +0200

    Add user agent to suppass Tilda connection reset

--- src/Service/FileImporter.php
+++ src/Service/FileImporter.php
@@ -2,7 +2,7 @@
 
 namespace App\Service;
 
-use ErrorException;
+use \ErrorException;
 use \RuntimeException;
 use Symfony\Component\HttpKernel\KernelInterface;
 use TildaTools\Tilda\Objects\Asset;
@@ -10,11 +10,12 @@ use TildaTools\Tilda\Objects\Asset;
 class FileImporter
 {
     private const TILDA_INCORRECT_PATH_MASK = '/https:\/\/tilda\.ws\/project(\d+)\/tilda-blocks-([0-9.]+)(js|css)/';
+    private const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0';
     private string $publicDirectory;
 
     public function __construct(KernelInterface $kernel)
     {
-        $this->publicDirectory = $kernel->getProjectDir() . '/public/';
+        $this->publicDirectory = $kernel->getProjectDir() . '/public';
     }
 
     public function import(Asset $asset, string $directory): void
@@ -36,7 +37,13 @@ class FileImporter
 
             throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
         });
-        $content = file_get_contents($asset->from);
+
+        $curlHandle=curl_init();
+        curl_setopt($curlHandle, CURLOPT_URL, $asset->from);
+        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curlHandle, CURLOPT_USERAGENT, self::DEFAULT_USER_AGENT);
+        $content = curl_exec($curlHandle);
+        curl_close($curlHandle);
         restore_error_handler();
 
         if ($content) {
